d02f086e1b6a62c36d6e53cd59300a1413dfe8da
Semendo250 <semendodouglas97@gmail.com>
2026-04-05 20:54:07 +0300

    Add About section toggle and Core Developer credits

--- script.js
+++ script.js
@@ -47,32 +47,43 @@ let avg;
 function averageGradeFunction(){
   avg=sum/12;
   if(avg>=80){
+    document.getElementById("res-grade").style.color="green"
     document.getElementById("res-grade").innerHTML="Grade: A"
+    
+    
 
   }
   else if(avg >=70 && avg<80){
     document.getElementById("res-grade").innerHTML="Grade: B"
+    document.getElementById("res-grade").style.color="blue"
   }
   else if(avg>=60 && avg<70){
     document.getElementById("res-grade").innerHTML="Grade: C"
+    document.getElementById("res-grade").style.color="pink"
   }
   else if(avg>=50 && avg<60){
     document.getElementById("res-grade").innerHTML="Grade: D"
+    document.getElementById("res-grade").style.color="yellow"
   }
   else if(avg>=40 && avg<50){
     document.getElementById("res-grade").innerHTML="Grade: E"
+    document.getElementById("res-grade").style.color="grey"
   }
   else if(avg<40){
     document.getElementById("res-grade").innerHTML="Grade: F"
+    document.getElementById("res-grade").style.color="red"
   }
   
 }
 
-function about(){
-  let about=document.getElementById("footContent").textContent="THIS PROGRAM IS DESIGNED BY DOUGLAS SEMENDO USING JAVASCRIPT,CASCADING STYLESHEET AND HYPERTEXT MARK-UP LANGUAGE. THE PROGRAM TAKES STUDENT SUBJECT-MARKS SCORED IN VARIOUS SUBJECTS AS INPUTS, AND THEN COMPUTES TO ASSIGN VARIOUS GRADES IN ACCORDANCE TO THE SCORE. THE SCORES ENTERED IS THEN USED TO DETERMINE THE GRADE WHICH IS LATER DISPLAYED IN FORM OF A CONFIRM MESSAGE WHICH POPS UP AS A NOTIFICATION. TWO SEPARATE PARAGRAPH ELEMENTS ARE USED TO DISPLAY THE TOTAL MARKS AND  THE AVERAGE MARK VALUE RESPECTIVELY AFTER CALCULATION. THERE IS ALSO THE USE OF BOTH LIBRARY FUNCTIONS SUCH AS parseInt() function AND USER-DEFINED FUNCTIONS WHICH ARE TRIGGERED TO FUNCTION UPON THE CLICKING OF THE BUTTONS."
- 
+function about() {
+  var x = document.getElementById("footContent");
+  if (x.style.display === "none" || x.style.display === "") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
-
 
 
 
